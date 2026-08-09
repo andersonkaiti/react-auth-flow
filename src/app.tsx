ee85d68adc53199b06cdd37f00833709a9ebba0b
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-09 02:34:05 -0300

    :sparkles: feat(http): add axios client and react-query setup

--- src/app.tsx
+++ src/app.tsx
@@ -2,21 +2,25 @@ import { AppBar } from '@components/app-bar'
 import { TooltipProvider } from '@components/ui/tooltip'
 import { AuthProvider } from '@contexts/auth-context'
 import { ThemeProvider } from '@contexts/theme-context'
+import { queryClient } from '@lib/query-client'
 import { Router } from '@router/index'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <TooltipProvider>
-            <AppBar />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <TooltipProvider>
+              <AppBar />
 
-            <Router />
-          </TooltipProvider>
-        </BrowserRouter>
-      </ThemeProvider>
-    </AuthProvider>
+              <Router />
+            </TooltipProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
