7940c6e3ef9dfeddc689987f78d21d80ef9b9237
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-09 16:48:28 -0300

    :sparkles: feat(query): toast global query errors

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -1,6 +1,15 @@
-import { QueryClient } from '@tanstack/react-query'
+import { toast } from '@components/ui/toast'
+import { QueryCache, QueryClient } from '@tanstack/react-query'
 
 export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast.add({
+        title: error.message,
+        type: 'error',
+      })
+    },
+  }),
   defaultOptions: {
     mutations: {
       retry: false,
