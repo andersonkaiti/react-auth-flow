efe227f7f2f36a1e85995db3b309fbe14e12bdaf
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-09 23:05:06 -0300

    :recycle: refactor(auth): move axios interceptors into AuthProvider to sync auth state

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -1,13 +1,16 @@
 import { toast } from '@components/ui/toast'
 import { storageKeys } from '@config/storage-keys'
+import { rotateRefreshToken } from '@http/auth/refresh-token'
 import {
   type ISignInRequest,
   signIn as signInRequest,
 } from '@http/auth/sign-in'
+import { httpClient } from '@http/http-client'
 import {
   createContext,
   type PropsWithChildren,
   useCallback,
+  useLayoutEffect,
   useState,
 } from 'react'
 
@@ -23,6 +26,55 @@ export function AuthProvider({ children }: PropsWithChildren) {
   const [isSignedIn, setIsSignedIn] = useState(
     () => !!localStorage.getItem(storageKeys.ACCESS_TOKEN_KEY),
   )
+
+  useLayoutEffect(() => {
+    const interceptorRequestId = httpClient.interceptors.request.use(
+      (config) => {
+        const accessToken = localStorage.getItem(storageKeys.ACCESS_TOKEN_KEY)
+
+        if (accessToken) {
+          config.headers.set('Authorization', `Bearer ${accessToken}`)
+        }
+
+        return config
+      },
+    )
+
+    const interceptorResponseId = httpClient.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const refreshToken = localStorage.getItem(storageKeys.REFRESH_TOKEN_KEY)
+
+        if (error.response.status !== 401 || !refreshToken) {
+          throw error
+        }
+
+        const originalRequest = error.config
+
+        if (originalRequest.url === '/refresh-token') {
+          localStorage.removeItem(storageKeys.ACCESS_TOKEN_KEY)
+          localStorage.removeItem(storageKeys.REFRESH_TOKEN_KEY)
+
+          setIsSignedIn(false)
+
+          throw error
+        }
+
+        const { accessToken, refreshToken: newRefreshToken } =
+          await rotateRefreshToken(refreshToken)
+
+        localStorage.setItem(storageKeys.ACCESS_TOKEN_KEY, accessToken)
+        localStorage.setItem(storageKeys.REFRESH_TOKEN_KEY, newRefreshToken)
+
+        return httpClient(originalRequest)
+      },
+    )
+
+    return () => {
+      httpClient.interceptors.request.eject(interceptorRequestId)
+      httpClient.interceptors.response.eject(interceptorResponseId)
+    }
+  }, [])
 
   const signIn = useCallback(async ({ email, password }: ISignInRequest) => {
     const { accessToken, refreshToken } = await signInRequest({
--- src/http/http-client.ts
+++ src/http/http-client.ts
@@ -1,48 +1,5 @@
-import { storageKeys } from '@config/storage-keys'
-import { rotateRefreshToken } from '@http/auth/refresh-token'
-import { routes } from '@router/routes'
 import axios from 'axios'
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
-
-httpClient.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem(storageKeys.ACCESS_TOKEN_KEY)
-
-  if (accessToken) {
-    config.headers.set('Authorization', `Bearer ${accessToken}`)
-  }
-
-  return config
-})
-
-httpClient.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const refreshToken = localStorage.getItem(storageKeys.REFRESH_TOKEN_KEY)
-
-    if ((error.response && error.response.status !== 401) || !refreshToken) {
-      throw error
-    }
-
-    const originalRequest = error.config
-
-    if (originalRequest.url === '/refresh-token') {
-      localStorage.removeItem(storageKeys.ACCESS_TOKEN_KEY)
-      localStorage.removeItem(storageKeys.REFRESH_TOKEN_KEY)
-
-      window.location.href = routes.signIn
-
-      throw error
-    }
-
-    const { accessToken, refreshToken: newRefreshToken } =
-      await rotateRefreshToken(refreshToken)
-
-    localStorage.setItem(storageKeys.ACCESS_TOKEN_KEY, accessToken)
-    localStorage.setItem(storageKeys.REFRESH_TOKEN_KEY, newRefreshToken)
-
-    return httpClient(originalRequest)
-  },
-)
